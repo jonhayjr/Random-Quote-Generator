28f0ebc55a9d2cd261175f8d2341b84da1b4206e
jonhayjr <jonhayjr@gmail.com>
2021-04-04 09:34:22 -0700

    Created print quote function

--- js/script.js
+++ js/script.js
@@ -15,29 +15,43 @@ const quotes = [
 quote: "Pain is temporary. It may last a minute, or an hour, or a day, or a year, but eventually it will subside and something else will take its place. If I quit, however, it lasts forever.",
 source: "Lance Armstrong",
 citation: "It's Not about the Bike: My Journey Back to Life",
-year: "2000"
+year: "2000",
+tags: ""
 },
 {
 quote: "Be yourself; everyone else is already taken.",
-source: "Oscar Wilde"
+source: "Oscar Wilde",
+citation: "",
+year: "",
+tags: ""
 },
 {
 quote: "You miss 100% of the shots you don’t take",
 source: "Wayne Gretzky",
-year: "1991"
+citation: "",
+year: "1991",
+tags: ""
 },
 {
 quote: "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
 source: "Maya Angelou",
+citation: "",
+year: "",
+tags: ""
 },
 {
 quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
 source: "Winston S. Churchill",
+citation: "",
+year: "",
 tags: "bravery, courage, failure, inspirational, success"
 },
 {
 quote: "Eighty percent of success is just showing up.",
-source: "Woody Allen"
+source: "Woody Allen",
+citation: "",
+year: "",
+tags: ""
 }
 ]
 
@@ -57,7 +71,39 @@ const getRandomQuote = () => {
 /***
  * `printQuote` function
 ***/
+const printQuote = () => {
+  //Stores quote from getRandomQuote function
+  const randomQuote = getRandomQuote();
 
+  let HTML = `
+  <p class="quote">${randomQuote.quote}</p>
+  <p class="source">${randomQuote.source}
+  `;
+  
+  //If object has citation property value, add HTML with value;
+  if (randomQuote.citation) {
+    HTML += `<span class="citation">${randomQuote.citation}</span>`;
+  }
+
+  //If object has year property value, add HTML with value.
+  if (randomQuote.year) {
+    HTML += `<span class="year">${randomQuote.year}</span>`;
+  }
+
+  //If object has tags property value, add HTML with value.
+  if (randomQuote.tags) {
+    HTML += `<span class="tags">Tags: ${randomQuote.tags}</span>`;
+  }
+
+  //Add closing p tag
+  HTML += '</p>';
+
+  //Grab element with id of quote-box
+  const quoteBox = document.querySelector('#quote-box');
+
+  //Update quoteBox HTML with HTML variable
+  quoteBox.innerHTML = HTML;
+}
 
 
 /***
